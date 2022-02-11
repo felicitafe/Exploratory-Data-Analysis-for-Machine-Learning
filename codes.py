8e54950fdcffb2accf1827956d9e3d300f6db3c2
felicitafe <86530586+felicitafe@users.noreply.github.com>
2022-02-11 14:38:36 +0300

    Update codes.py

--- codes.py
+++ codes.py
@@ -1,6 +1,15 @@
 import pandas as pd
 
 filepath = 'data/iris-write-from-docker.csv'
+
+# read JSON file as dataframe
 data = pd.read_csv(filepath)
 print(data.iloc[:5])
+
+
+# write dataframe file to JSON
+
+data.to_jason('outputfile.json')
+
+
 
